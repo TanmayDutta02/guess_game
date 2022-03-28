61aa0defe08e0b80d48e7d23de0e4a3f65cbbd15
TANMAY DUTTA <91477406+TanmayDutta02@users.noreply.github.com>
2022-03-28 18:57:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = “UTF-8”>
+	<title>Tanmay Dutta</title>
+</head>
+<body>
+  <h1> Welcome to my guessing game</h1>
+
+  <?php
+  $correctnumber=19;
+  if(isset($_GET['guess']))
+  {
+    if (is_numeric($_GET['guess'])===FALSE)
+  {
+  echo "Your guess is not a number";
+  }
+  else if ($_GET['guess']<$correctnumber)
+  {
+  echo "Your guess is too low";
+  }
+  else if ($_GET['guess']>$correctnumber)
+  {
+  echo "Your guess is too high";
+  }
+  else if ($_GET['guess']==$correctnumber)
+  {
+  echo "Congratulations - You are right";
+  }
+  }
+  else
+  {
+  echo "Missing guess parameter <br> Your guess is too short";
+  }
+  ?>
+</body>
+</html>
